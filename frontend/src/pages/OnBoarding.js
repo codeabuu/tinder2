da032a653856a436ad1c8509e1570f7a45798eb7
Abdullahi Ngui <125456974+codeabuu@users.noreply.github.com>
2024-03-23 09:57:02 -0400

    Update OnBoarding.js

--- frontend/src/pages/OnBoarding.js
+++ frontend/src/pages/OnBoarding.js
@@ -27,7 +27,7 @@ const OnBoarding = () => {
         console.log('submitted')
         e.preventDefault()
         try {
-            const response = await axios.put('https://tinder2-snowy.vercel.app/user', {formData})
+            const response = await axios.put('http://localhost:3000/user', {formData})
             console.log(response)
             const success = response.status === 200
             if (success) navigate('/dashboard')
